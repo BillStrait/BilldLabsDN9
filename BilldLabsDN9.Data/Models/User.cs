0d84776a123f328c2d37e509d11777da1e8b57d1
BillStrait <bill.strait@gmail.com>
2025-01-26 16:23:51 -0600

    Added entity framework, the initial user, and confirmed working.

---
+++ BilldLabsDN9.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilldLabsDN9.Data.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string? Email { get; set; }
+        public DateTime JoinDate { get; set; }
+        public DateTime? SuspendDate { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsContributor { get; set; }
+        public string? PassKeyPublicKey { get; set; }
+    }
+}
